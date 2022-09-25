585e9fe3cd997f23e749d98cbb048611af8c82b7
Marian Remoroza <mrremoroza@cpp.edu>
2022-09-24 17:32:27 -0700

    initial commit

---
+++ program1.py
@@ -0,0 +1,21 @@
+'''
+Marian Remoroza
+CS 2410 Data Science
+Program 1: Python Control Structures, Functions, and Text File Processin
+'''
+# Instructions: Given the rainfall.txt file, which consists of a city name and a rainfall value in inches,
+# write a python codes for the following tasks:
+#   1. A function that returns (city, rainfall_value) for the city with the highest/largest rainfall;
+#   2. A function that returns (city, rainfall_value) for the city with the lowest/smallest rainfall;
+#   3. A function that returns the mean rainfall value;
+#   4. A function that returns the number of cities with rainfall greater than the mean value;
+#   5. A main function that opens the text file, reads in data (each line is a city and its rainfall in inches),
+#      convert the rainfall data to centimeter, then call each of the above function to get the required result.
+#      Display each result in proper format and in the unit of centimeters.
+#   6. Call the main function to run the program
+
+#!/usr/bin/python
+import pandas as pd
+
+reader = pd.read_csv("rainfall.txt", delimiter=" ", header=None).to_dict()[0]
+print(reader)

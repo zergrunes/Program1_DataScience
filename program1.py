64244e87158568ed475b21bb497ea8f13fd66faf
Marian Remoroza <mrremoroza@cpp.edu>
2022-09-25 18:53:01 -0700

    added comments

--- program1.py
+++ program1.py
@@ -5,30 +5,35 @@ Program 1: Python Control Structures, Functions, and Text File Processing
 '''
 #!/usr/bin/python
 import pandas as pd
+
 cm = 0
-rainfall = open("rainfall.txt", "r")
-rf_cm = open("rainfall_cm.txt", "w")
-for line in rainfall:
+rainfall = open("rainfall.txt", "r")  # opening rainfall.txt
+rf_cm = open("rainfall_cm.txt", "w")  # new file for cm values
+
+for line in rainfall:  # converting rainfall.txt values to cm and then writing it to a new file
     values = line.split()
     inches = float(values[1])
-    cm = inches*2.54
+    cm = inches*2.54  # conversion
     rf_cm.write(f"{values[0]} {cm}\n")
 rainfall.close()
 rf_cm.close()
 
 df = pd.read_csv("rainfall_cm.txt", sep=" ", header=None,
-                 names=["City", "Rainfall Value"])
+                 names=["City", "Rainfall Value"])  # OG rainfall file
 
 df2 = pd.read_csv("rainfall.txt", sep=" ", header=None,
-                  names=["City", "Rainfall Value in Inches"])
+                  names=["City", "Rainfall Value (in)"])  # rainfall file to use for output
 print(df2)
-# highest rainfall
-high_rf = df.max()
-print("City with the highest rainfall:\n", high_rf)
-# lowest rainfall
-low_rf = df.min()
+
+high_rf = df.loc[df["Rainfall Value"].idxmax()]  # highest rainfall
+print("City with the most amount of rainfall:\n", high_rf)
+
+low_rf = df.min()   # lowest rainfall
 print("City with the least amount of rainfall:\n", low_rf)
-# mean rainfall
-rf_mean = df['Rainfall Value'].mean()
-print("The mean rainfall value is:", rf_mean, "cm")
+
+rf_mean = df['Rainfall Value'].mean()   # mean rainfall
+rf = "{:.2f}".format(rf_mean)
+print("The mean rainfall value is:", rf, "cm")
 # number of cities with rainfall greater than the mean
+# count values in one column w/ condition: len(df[df['Rainfall Value']==])
+# print("There are ", , " of cities greater than the mean rainfall value.")
